5e6f53e0fc58e6da144355aa99716ddd07bc98e8
Sreejith K <sreejithemk@gmail.com>
2010-07-12 14:31:04 +0530

    Added macro.py and README

---
+++ macro.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python
+#
+# Macro substitution using regular expressions
+#
+# @author: Sreejith K
+# Created On 12 July 2010
+
+import sys
+import os
+import re
+
+class Macro(object):
+    def __init__(self, mapper={}, content=''):
+        self.mapper = mapper
+        self.content = content
+
+    def substitute_all(self):
+        for regex, replace_string in self.mapper.iteritems():
+            self.content = re.sub(regex, replace_string, self.content)
+        return self.content
+
+    def substitute(self, regex):
+        self.content = re.sub(regex, replace_string, self.content)
+        return self.content
+
+    def get_content(self):
+        return self.content
+
+    def __str__(self):
+        if self.content:
+            return self.content
+        else:
+            return 'No content provided'
+
+
+if __name__ == '__main__':
+    mapper = {
+            '\[TYPE\]': 'System',
+            '\[OS\]': 'Linux',
+            '\[VERSION\]': '2.6'
+            }
+
+    content = """
+    My [TYPE] configuration:
+        Operating System: [OS]
+        Kernel Version: [VERSION]
+        [OS] is POSIX compliant
+    """
+    print 'Content:\n', content
+    macro = Macro(mapper, content)
+
+    result = macro.substitute_all()
+
+    print 'Result:\n', result
